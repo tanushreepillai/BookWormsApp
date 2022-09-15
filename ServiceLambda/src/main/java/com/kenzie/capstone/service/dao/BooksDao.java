cfd7f950273d6a616ff6c85a1476c0ae76e78b26
ericjbergan <ericjbergan@gmail.com>
2022-09-15 10:10:11 -0500

    lambda files partially done

---
+++ ServiceLambda/src/main/java/com/kenzie/capstone/service/dao/BooksDao.java
@@ -0,0 +1,13 @@
+package com.kenzie.capstone.service.dao;
+
+import com.kenzie.capstone.service.model.BooksRecord;
+
+import java.util.List;
+
+public class BooksDao {
+
+    public List<BooksRecord> getBooksData() {
+
+        return null;
+    }
+}
