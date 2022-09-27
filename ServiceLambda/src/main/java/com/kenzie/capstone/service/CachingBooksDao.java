7bc26e8d3835fd9211022580f3494d39131a7a08
Marc-Dwayne Santos <marcdwayne.santos@mykenzie.snhu.edu>
2022-09-27 19:33:31 -0400

    Caching fixes; implemented in-memory caching instead of distributed caching

---
+++ ServiceLambda/src/main/java/com/kenzie/capstone/service/CachingBooksDao.java
@@ -0,0 +1,27 @@
+package com.kenzie.capstone.service;
+
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
+import com.google.common.cache.LoadingCache;
+import com.kenzie.capstone.service.dao.BooksDao;
+import com.kenzie.capstone.service.model.BooksData;
+import com.kenzie.capstone.service.model.BooksRecord;
+
+import javax.inject.Inject;
+import java.util.List;
+
+public class CachingBooksDao {
+
+    private final LoadingCache<String, List<BooksRecord>> bookCache;
+
+    @Inject
+    public CachingBooksDao(BooksDao booksDao) {
+        bookCache = CacheBuilder.newBuilder()
+                .build(CacheLoader.from(booksDao::getBooksData));
+    }
+
+    public List<BooksRecord> getBooksData(String id) {
+        return bookCache.getUnchecked(id);
+    }
+
+}
--- ServiceLambda/src/main/java/com/kenzie/capstone/service/dependency/DaoModule.java
+++ ServiceLambda/src/main/java/com/kenzie/capstone/service/dependency/DaoModule.java
@@ -1,8 +1,10 @@
 package com.kenzie.capstone.service.dependency;
 
 
+import com.kenzie.capstone.service.CachingBooksDao;
 import com.kenzie.capstone.service.dao.BooksDao;
 import com.kenzie.capstone.service.dao.ExampleDao;
+import com.kenzie.capstone.service.model.BooksData;
 import com.kenzie.capstone.service.util.DynamoDbClientProvider;
 
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
@@ -33,6 +35,14 @@ public class DaoModule {
     @Inject
     public BooksDao provideBooksDao(@Named("DynamoDBMapper") DynamoDBMapper mapper) {
         return new BooksDao(mapper);
+    }
+
+    @Singleton
+    @Provides
+    @Named("CachingBooksDao")
+    @Inject
+    public CachingBooksDao provideCachingBooksDao(@Named("BooksDao")BooksDao booksDao) {
+        return new CachingBooksDao(booksDao);
     }
 
 }
