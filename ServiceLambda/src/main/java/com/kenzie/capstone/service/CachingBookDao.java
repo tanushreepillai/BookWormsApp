2f21d2ea0ee3913aac4c97aaf5b099c5f32e350b
Marc-Dwayne Santos <marcdwayne.santos@mykenzie.snhu.edu>
2022-09-27 14:34:40 -0400

    Team collaboration on Backend; Caching (though don't know what to cache at this point in time)

---
+++ ServiceLambda/src/main/java/com/kenzie/capstone/service/CachingBookDao.java
@@ -0,0 +1,31 @@
+package com.kenzie.capstone.service;
+
+import com.kenzie.capstone.service.dao.BooksDao;
+import com.kenzie.capstone.service.model.BooksData;
+import com.kenzie.capstone.service.model.BooksRecord;
+
+import javax.inject.Inject;
+import java.util.List;
+
+public class CachingBookDao {
+    private final BooksDao booksDao;
+    private final CacheClient cache;
+
+    @Inject
+    public CachingBookDao(BooksDao booksDao, CacheClient cache) {
+        this.booksDao = booksDao;
+        this.cache = cache;
+    }
+
+    public String getBook(String id) {
+        String booksData = cache.getValue(id)
+//                .map(String::valueOf)
+                .orElseGet(() -> booksDao.getBooksData(id).get(0).getTitle());
+        cache.setValue(id,300, booksData.toString());
+        return booksData;
+    }
+
+    public boolean bookInvalidated(String id) {
+        return cache.invalidate(id);
+    }
+}
