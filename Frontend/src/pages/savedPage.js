23e5d51738bf44648fc74ad10cdd8cbf21e339b5
Candace Choe <candacehchoe@gmail.com>
2022-10-11 12:29:14 -0700

    selecting buttons now

--- Frontend/src/pages/savedPage.js
+++ Frontend/src/pages/savedPage.js
@@ -55,7 +55,6 @@ class SavedPage extends BaseClass {
                 <div>Description: ${book.description}</div>
                 <button id="delete" name="${book.bookId}" data-index="${i}">Delete book</button>
                 <button id="update" name="${book.bookId}" data-index="${i}">Update book</button>
-
                 `
             }
 
