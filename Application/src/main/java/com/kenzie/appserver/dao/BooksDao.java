4a79d98cb4b2131bcc47151ce86e078d842e97a5
Candace Choe <candacehchoe@gmail.com>
2022-10-11 19:03:30 -0700

    working on booksdao with marc

--- Application/src/main/java/com/kenzie/appserver/dao/BooksDao.java
+++ Application/src/main/java/com/kenzie/appserver/dao/BooksDao.java
@@ -1,10 +1,15 @@
 package com.kenzie.appserver.dao;
 
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBScanExpression;
+import com.amazonaws.services.dynamodbv2.datamodeling.PaginatedScanList;
 import com.kenzie.appserver.backend.models.Books;
 import com.kenzie.appserver.repositories.model.BooksRecord;
 
 import javax.inject.Inject;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public class BooksDao {
     private DynamoDBMapper mapper;
@@ -24,5 +29,26 @@ public class BooksDao {
                 bookRecord.getFinishedReading(),
                 id);
     }
+
+    public Set<Books> getBooks() {
+        Set<BooksRecord> records = mapper.scan(BooksRecord.class, new DynamoDBScanExpression()).stream().collect(Collectors.toSet());
+        Set<Books> returnedBooks = new HashSet<>();
+        for (BooksRecord bookRecord : records) {
+            new Books(bookRecord.getImageLink(),
+                    bookRecord.getDescription(),
+                    bookRecord.getAuthor(),
+                    bookRecord.getTitle(),
+                    bookRecord.getFinishedReading(),
+                    bookRecord.getBookId());
+        }
+        return returnedBooks;
+        //        Books book = new Books(); // for selecting a book with particular attributes
+        //            song.setArtist(artist);
+        //        DynamoDBQueryExpression<Song> queryExpression = new DynamoDBQueryExpression<Song>()
+        //                .withHashKeyValues(song);
+        //        PaginatedQueryList<Song> songList = mapper.query(Song.class, queryExpression);
+        //            return songList;
+    }
+
 
 }
--- Application/src/main/java/com/kenzie/appserver/dao/CachingBooksDao.java
+++ Application/src/main/java/com/kenzie/appserver/dao/CachingBooksDao.java
@@ -3,30 +3,40 @@ package com.kenzie.appserver.dao;
 import com.google.common.cache.CacheBuilder;
 import com.google.common.cache.CacheLoader;
 import com.google.common.cache.LoadingCache;
+import com.google.common.collect.Iterables;
 import com.kenzie.appserver.backend.models.Books;
 import com.kenzie.appserver.dao.BooksDao;
 
 import javax.inject.Inject;
 import java.util.HashSet;
+import java.util.Optional;
 import java.util.Set;
 import java.util.stream.Collectors;
 
 public class CachingBooksDao {
 
-    private final LoadingCache<String, Books> bookCache;
+    private final LoadingCache<String, Set<Books>> bookCache; // return all books in the cache (values)
+
+                                // Set<Books> => only want to return the first item
 
     @Inject
     public CachingBooksDao(BooksDao booksDao) {
         bookCache = CacheBuilder.newBuilder()
-                .build(CacheLoader.from(booksDao::getBook));
+                .build(CacheLoader.from(booksDao::getBooks));
     }
 
-    public Books getBook(String id) {
-        return bookCache.getUnchecked(id);
-    }
+    //    public Books getBook(String id) { // title+author.charAt(0)
+    //        return bookCache.getUnchecked(id);
+    //    }
+
+    // getBook method: helper method for getBooks
 
     public Set<Books> getBooks() {
-        return bookCache.asMap().values().stream().collect(Collectors.toSet());
+        Set<Set<Books>> bookSet = bookCache.asMap().values().stream().collect(Collectors.toSet());
+        Optional<Set<Books>> firstElements = bookSet.stream().findFirst();
+        if (firstElements.isPresent()) {
+            return firstElements.get();
+        }
+        return new HashSet<>();
     }
-
 }
